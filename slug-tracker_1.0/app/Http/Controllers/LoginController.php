c1a64ea7146d94de0620729008a9eaceb391d9bb
venkatlokeswar <thisizlokesh@gmail.com>
2017-04-18 15:50:32 +0530

    Laravel Setup
    * Intial commit
    
    * Added login route

---
+++ slug-tracker_1.0/app/Http/Controllers/LoginController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+
+class LoginController extends Controller
+{
+    function index(){
+         $this->data['name'] = 'Slug';
+         return view('login.index',$this->data);
+    }
+}
